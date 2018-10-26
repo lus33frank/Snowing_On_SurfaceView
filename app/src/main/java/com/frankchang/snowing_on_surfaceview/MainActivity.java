2a908eee57801286187160086d8c28fe94cd9198
lus33frank <lus33frank@gmail.com>
2018-10-27 00:08:00 +0800

    Commit : 1    專案初始化版本。 在 SurfaceView 上畫出下雪動畫

---
+++ app/src/main/java/com/frankchang/snowing_on_surfaceview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.frankchang.snowing_on_surfaceview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
